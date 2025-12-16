600a8fb3e6965c5999115412e7f1f63f5f89e751
g <g>
2025-12-16 16:33:28 +0100

    zad2

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 using namespace std;
 
-/*zadanei 2
-int pole(int, int);
+struct trojkat {
 
+};
+
+
+
+
+/*zadanie 2
 struct Prostokat {
     int a = 10;
     int b = 24;
 }boki;
 
+int pole(Prostokat);
+
 int main() {
 
-    cout << "Pole prostokata o bokach " << boki.a << " i " << boki.b << " wynosi: " << pole(boki.a, boki.b) << endl;
+    cout << "Pole prostokata o bokach " << boki.a << " i " << boki.b << " wynosi: " << pole(boki) << endl;
 
     return 0;
 }
 
-int pole(int a, int b) {
-    return a * b;
+int pole(Prostokat pol) {
+    return pol.a * pol.b;
 }
 */
+
 
 /*zadanie 1
 struct Wspolrzedne {
