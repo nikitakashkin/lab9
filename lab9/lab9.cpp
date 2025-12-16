758c578c209654836a14b933e7a76f99626537af
g <g>
2025-12-16 16:01:03 +0100

    zadanie2

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+/*zadanei 2
+int pole(int, int);
 
+struct Prostokat {
+    int a = 10;
+    int b = 24;
+}boki;
+
+int main() {
+
+    cout << "Pole prostokata o bokach " << boki.a << " i " << boki.b << " wynosi: " << pole(boki.a, boki.b) << endl;
+
+    return 0;
+}
+
+int pole(int a, int b) {
+    return a * b;
+}
+*/
 
 /*zadanie 1
 struct Wspolrzedne {
